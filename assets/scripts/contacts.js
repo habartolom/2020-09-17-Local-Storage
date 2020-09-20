b4594516d3bbae96c1625389bd31947b834f14ba
Harold Andrrés Bartolo Moscoso <habartolom@gmail.com>
2020-09-20 00:26:01 -0500

    Local Storage

--- assets/scripts/contacts.js
+++ assets/scripts/contacts.js
@@ -1,22 +1,22 @@
 const contactsList = document.getElementById('contactsList');
 
 let contactsArray = JSON.parse(localStorage.getItem('contacts'));
-contactsArray.sort(function(a, b){
-    if(a.name < b.name)
-        return -1;
-    if(a.name < b.name)
-        return 1;
-    return 0;
-});
-
-console.log(contactsArray);
-
-for(let i = 0; i < contactsArray.length; i++){
-    contactsList.innerHTML += `
-    <div class='contact'>
+if(contactsArray != null){
+    contactsArray.sort(function(a, b){
+        if(a.name < b.name)
+            return -1;
+        if(a.name < b.name)
+            return 1;
+        return 0;
+    });
+    
+    for(let i = 0; i < contactsArray.length; i++){
+        contactsList.innerHTML += `
+        <div class='contact'>
         <h5>${contactsArray[i].name}</h5>
         <div>${contactsArray[i].address}</div>
         <div>${contactsArray[i].phone}</div>
-    </div>
-    `
+        </div>
+        `
+    }
 }
